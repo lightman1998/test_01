9e2e355cda41184aeb0748f0afb4095a7f4227c7
lightman1998 <kall-lightman@yandex.ru>
2017-10-09 12:33:16 +0300

    Update test_02.cpp

--- test_02/test_02.cpp
+++ test_02/test_02.cpp
@@ -3,21 +3,18 @@
 using namespace std;
 
 template <typename Iterator, typename T>
-auto find_two_elements_with_sum(Iterator first, Iterator last, T c)->std::pair<bool, std::pair<Iterator, Iterator>>
-{
-    bool answer = false;
+auto find_two_elements_with_sum(Iterator first, Iterator last, T c)->std::pair<bool, std::pair<Iterator, Iterator>> {
+bool answer = false;
     int foo[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    while(first!=last)
-    {
+    while(first!=last) {
         int Sum = foo[first] + foo[last];
 
         if(Sum < c)
             first++;
         else if(Sum > c)
             last--;
-        else
-        {
+        else {
             answer = true;
             cout << "bool = " << answer << endl;
             cout << "iterators: " << first << " " << last <<endl;
@@ -25,12 +22,10 @@ auto find_two_elements_with_sum(Iterator first, Iterator last, T c)->std::pair<b
         }
     }
     cout << "No more found..." <<endl;
-//}
 
 }
 
-int main()
-{
+int main() {
     int X;
     cin >> X;
     find_two_elements_with_sum(0, 9, X);
